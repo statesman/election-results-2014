6dc67bbb637cc26cf6607e2b36b7fec4413f27e7
Andrew Chavez <adchavez@gmail.com>
2014-11-06 11:04:56 -0600

    Fixes #1. Comma-fies result totals

--- src/js/results.js
+++ src/js/results.js
@@ -1,4 +1,4 @@
-var Results = (function($, Handlebars) {
+var Results = (function($, Handlebars, _, numeral) {
 
   function Results(el) {
     this.$el = $(el);
@@ -13,9 +13,12 @@ var Results = (function($, Handlebars) {
 
   // Update the key with new data
   Results.prototype.update = function(data) {
+    _.each(data.results, function(result) {
+      result.votes = numeral(result.votes).format('0,0');
+    });
     this.$el.html(this.template(data));
   };
 
   return Results;
 
-}(jQuery, Handlebars));
+}(jQuery, Handlebars, _, numeral));
